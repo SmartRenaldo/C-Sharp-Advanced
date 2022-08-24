b103e65ec346f015eb93a8e2d954659028ed36f4
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 10:01:29 +0930

    descructor

---
+++ 02_Descructor/02_Descructor/Dog.cs
@@ -0,0 +1,15 @@
+﻿namespace _02_Descructor
+{
+    public class Dog
+    {
+        public Dog()
+        {
+            Console.WriteLine("I'm a dog");
+        }
+
+        public static void Bark()
+        {
+            Console.WriteLine("Wang");
+        }
+    }
+}
---
+++ 02_Descructor/02_Descructor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_Descructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog.Bark();
+            Dog d = new Dog();
+        }
+    }
+}
