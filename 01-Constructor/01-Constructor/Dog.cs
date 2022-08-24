e1e6c234ea2313bd739a498e5759d80913b49417
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 10:00:40 +0930

    Constructor

---
+++ 01-Constructor/01-Constructor/Dog.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_Constructor
+{
+    public class Dog
+    {
+        public Dog()
+        {
+            Console.WriteLine("I'm a dog");
+        }
+
+        public static void Bark()
+        { 
+            Console.WriteLine("Wang");
+        }
+    }
+}
---
+++ 01-Constructor/01-Constructor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog.Bark();
+            Dog d = new Dog();
+        }
+    }
+}
