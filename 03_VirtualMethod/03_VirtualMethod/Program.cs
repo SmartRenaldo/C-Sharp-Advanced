c194863f1f557ba839ccce521091b5d83091987c
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 10:46:31 +0930

    virtual method

---
+++ 03_VirtualMethod/03_VirtualMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03_VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualMethod method = new VirtualMethod();
+            int res = method.Calculate(2, 5);
+            Console.WriteLine($"2 + 5 = {res}");
+
+            VirtualMethodChild child = new VirtualMethodChild();
+            int res2 = child.Calculate(2, 5);
+            Console.WriteLine($"2 * 5 = {res2}");
+
+            VirtualMethodChild2 child2 = new VirtualMethodChild2();
+            int res3 = child2.Calculate(2, 5);
+            Console.WriteLine($"2 - 5 = {res3}");
+        }
+    }
+}
---
+++ 03_VirtualMethod/03_VirtualMethod/VirtualMethod.cs
@@ -0,0 +1,26 @@
+﻿namespace _03_VirtualMethod
+{
+    public class VirtualMethod
+    {
+        public virtual int Calculate(int a, int b) 
+        { 
+            return a + b;
+        }
+    }
+
+    class VirtualMethodChild: VirtualMethod
+    {
+        public override int Calculate(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class VirtualMethodChild2: VirtualMethod
+    {
+        public override int Calculate(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
