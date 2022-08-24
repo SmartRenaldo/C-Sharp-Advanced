6de46250ee06a3d58abdd402452e7b5d4ab7aec1
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 13:44:28 +0930

    genericity

---
+++ 06-Genericity/06-Genericity/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06_Genericity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "aaa", "bbb", "", "rrr" };
+
+            foreach (string item in list)
+            {
+                if (string.IsNullOrWhiteSpace(item))
+                {
+                    continue;
+                }
+
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
