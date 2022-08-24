c07f895350d7519262ecbb5ceb62288edc9720ec
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 14:36:33 +0930

    genericity method

---
+++ 09-GenericityMethod/09-GenericityMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _09_GenericityMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = Display<string>("Tesla");
+            Console.WriteLine(value);
+        }
+
+        public static T Display<T>(T value)
+        {
+            return value;
+        }
+    }
+}
