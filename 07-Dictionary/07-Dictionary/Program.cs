f5652a1dbea87dca898fe3db7c23249cbe50f1f6
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 14:06:33 +0930

    dictionary

---
+++ 07-Dictionary/07-Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //key cannot be repeated
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(0, "cat");
+            dictionary.Add(1, "dog");
+            dictionary.Add(2, "monkey");
+
+            foreach (KeyValuePair<int, string> pair in dictionary)
+            {
+                Console.WriteLine($"Key: {pair.Key.ToString()}, Vaule: {pair.Value.ToString()}");
+            }
+
+            Console.WriteLine(dictionary.ContainsValue("dog"));
+            Console.WriteLine(dictionary.ContainsValue("panda"));
+        }
+    }
+}
