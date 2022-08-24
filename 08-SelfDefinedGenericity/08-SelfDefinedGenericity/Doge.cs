76e785c2c54d431167a0285d7685a4a34a5c2c91
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 14:36:05 +0930

    self defined genericit

---
+++ 08-SelfDefinedGenericity/08-SelfDefinedGenericity/Doge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_SelfDefinedGenericity
+{
+    public class Doge<T>
+    {
+        private T Value;
+
+        public Doge(T value)
+        {
+            Value = value;
+        }
+
+        public T GetValue()
+        {
+            return Value;
+        }
+    }
+}
---
+++ 08-SelfDefinedGenericity/08-SelfDefinedGenericity/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _08_SelfDefinedGenericity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doge<string> doge1 = new Doge<string>("Dogecoin");
+            Console.WriteLine(doge1.GetValue());
+
+            Doge<int> doge2 = new Doge<int>(2);
+            Console.WriteLine(doge2.GetValue());
+        }
+    }
+}
