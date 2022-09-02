9edf969b02c08ce52258672c5274bd5fde92665e
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-02 22:33:52 +0930

    create obj with param by reflection

---
+++ 14_CreateObjWithParamByReflection/14_CreateObjWithParamByReflection/DbSQLite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_CreateObjWithParamByReflection
+{
+    public class DbSQLite : DbInterface
+    {
+        private string _sql;
+        private int value;
+
+        public DbSQLite()
+        {
+            Console.WriteLine("Constructor {0}", this.GetType().Name);
+        }
+
+        public DbSQLite(string sql)
+        {
+            _sql = sql;
+            Console.WriteLine("Constructor {0} with a {1} parameter", this.GetType().Name, sql.GetType());
+        }
+
+        public DbSQLite(int value)
+        {
+            this.value = value;
+            Console.WriteLine("Constructor {0} with a {1} parameter", this.GetType().Name, value.GetType());
+        }
+
+        public DbSQLite(string sql, int value)
+        {
+            this._sql = sql;
+            this.value = value;
+            Console.WriteLine("Constructor {0} with {1} and {2} parameters", 
+                this.GetType().Name, sql.GetType(), value.GetType());
+        }
+
+        public void Query()
+        {
+            Console.WriteLine("{0}.Query", this.GetType().Name);
+        }
+    }
+}
---
+++ 14_CreateObjWithParamByReflection/14_CreateObjWithParamByReflection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_CreateObjWithParamByReflection
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("----------create an object with parameters by reflection---------");
+
+                Assembly assembly = Assembly.LoadFrom("14_CreateObjWithParamByReflection.dll");
+
+                Type type = assembly.GetType("_14_CreateObjWithParamByReflection.DbSQLite");
+                
+                foreach(ConstructorInfo constructor in type.GetConstructors())
+                {
+                    Console.WriteLine(constructor.Name);
+
+                    foreach (var param in constructor.GetParameters())
+                    {
+                        Console.WriteLine(param.ParameterType);
+                    }
+                }
+
+                DbSQLite dbSQLite = Activator.CreateInstance(type) as DbSQLite;
+                DbSQLite dbSQLite2 = Activator.CreateInstance(type, new object[] {"select * from students"}) as DbSQLite;
+                DbSQLite dbSQLite3 = Activator.CreateInstance(type, new object[] {25}) as DbSQLite;
+                DbSQLite dbSQLite4 = Activator.CreateInstance(type, new object[] {"select * from students", 25}) as DbSQLite;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
