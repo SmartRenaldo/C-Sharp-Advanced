779704f198311a6f6e41fee868607e41532e3182
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-02 12:14:55 +0930

    13_CreateObjByReflection

---
+++ 13_CreateObjByReflection/13_CreateObjByReflection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_CreateObjByReflection
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("----------create object by reflection---------");
+
+                Assembly assembly;
+                assembly = Assembly.LoadFrom("13_CreateObjByReflection.dll");
+
+                Type type = assembly.GetType("_13_CreateObjByReflection.DbSQLite");
+                DbSQLite dbSQLite = Activator.CreateInstance(type) as DbSQLite;
+
+                dbSQLite.Query();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
