81254ebf0491012b1bb2ed2c7afb1cae83de7ba2
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-03 19:50:41 +0930

    attribute

---
+++ 20_Attribute/20_Attribute/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AttributeClass attributeClass = new AttributeClass();
+            attributeClass.HandleMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
