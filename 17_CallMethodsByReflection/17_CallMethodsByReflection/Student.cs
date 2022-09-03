a3655f92ab4a164a29dc19eda4813e8f2650f970
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-03 10:45:35 +0930

    Call Methods By Reflection

---
+++ 17_CallMethodsByReflection/17_CallMethodsByReflection/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_CallMethodsByReflection
+{
+    public class Student
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method 1 for {0}", this.GetType());
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method 2 for {0}", this.GetType());
+        }
+
+        public void Method2(string s)
+        {
+            Console.WriteLine("Method 2 for {0}", this.GetType());
+            Console.WriteLine("/t" + s);
+        }
+
+        public void Method2(string s, int i)
+        {
+            Console.WriteLine("Method 2 for {0}", this.GetType());
+            Console.WriteLine("/t" + s);
+            Console.WriteLine("/t" + i);
+        }
+
+        private void Method3()
+        {
+            Console.WriteLine("Method 3 for {0}", this.GetType());
+        }
+
+        private void Method3(int i)
+        {
+            Console.WriteLine("Method 3 for {0}", this.GetType());
+            Console.WriteLine("/t" + i);
+        }
+
+        public static void Method3(string s)
+        {
+            Console.WriteLine("Method 3 for {0}", typeof(Student));
+            Console.WriteLine("/t" + s);
+        }
+    }
+}
