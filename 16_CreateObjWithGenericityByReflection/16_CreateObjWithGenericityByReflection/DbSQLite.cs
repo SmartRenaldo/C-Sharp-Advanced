f3b2c9ea2e6765cd0e0055c9f79f3f5951baaead
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-03 10:30:17 +0930

    Create Obj With Genericity By Reflection

---
+++ 16_CreateObjWithGenericityByReflection/16_CreateObjWithGenericityByReflection/DbSQLite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_CreateObjWithGenericityByReflection
+{
+    public class DbSQLite<T, W, S> : DbInterface
+    {
+        private string _sql;
+        private int value;
+
+        public DbSQLite()
+        {
+            Console.WriteLine(1111111);
+            Console.WriteLine("Constructor {0}", this.GetType().Name);
+        }
+
+        public void Query()
+        {
+            Console.WriteLine("{0}.Query", this.GetType().Name);
+        }
+    }
+}
---
+++ 16_CreateObjWithGenericityByReflection/16_CreateObjWithGenericityByReflection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_CreateObjWithGenericityByReflection
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("----------create an object with private constructors by reflection---------");
+
+                Assembly assembly = Assembly.LoadFrom("16_CreateObjWithGenericityByReflection.dll");
+
+                // `3 means there are 3 genericity types
+                Type type = assembly.GetType("_16_CreateObjWithGenericityByReflection.DbSQLite`3");
+                Type genericityType = type.MakeGenericType(new Type[] { typeof(int), typeof(double), typeof(bool) });
+
+                DbSQLite<int, double, bool> dbSQLite = Activator.CreateInstance(genericityType) as DbSQLite<int, double, bool>;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
