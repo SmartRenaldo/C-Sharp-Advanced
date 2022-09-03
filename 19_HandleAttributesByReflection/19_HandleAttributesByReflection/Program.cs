a1b15f3a92c2589d71f4c04eb975592fa3f75c14
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-03 14:11:24 +0930

    Handle Attributes By Reflection

--- 19_HandleAttributesByReflection/19_HandleAttributesByReflection/Program.cs
+++ 19_HandleAttributesByReflection/19_HandleAttributesByReflection/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_HandleAttributesByReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------Handle Attributes by Reflection-------------");
+
+            Student s = new Student()
+            {
+                Id = 1,
+                Name = "TARA",
+                Address = "Campbelltown",
+                age = 17
+            };
+
+            Assembly assembly = Assembly.LoadFrom("19_HandleAttributesByReflection.dll");
+            Type type = assembly.GetType("_19_HandleAttributesByReflection.Student");
+            Student student = Activator.CreateInstance(type) as Student;
+
+            Console.WriteLine("Properties:");
+            foreach (var prop in type.GetProperties())
+            {
+                Console.WriteLine($"{prop.PropertyType} -- {prop.Name} -- {prop.GetValue(s)}");
+            }
+
+            foreach (var prop in type.GetProperties())
+            {
+                if (prop.Name.Equals("Id"))
+                {
+                    prop.SetValue(s, 250);
+                }
+                else if (prop.Name.Equals("Name"))
+                {
+                    prop.SetValue(s, "AWESOME");
+                }
+                else if (prop.Name.Equals("Address"))
+                {
+                    prop.SetValue(s, "Adelaide");
+                }
+            }
+
+            foreach (var prop in type.GetProperties())
+            {
+                Console.WriteLine($"{prop.PropertyType} -- {prop.Name} -- {prop.GetValue(s)}");
+            }
+
+            Console.WriteLine("Fields:");
+            foreach (var field in type.GetFields())
+            {
+                Console.WriteLine($"{field.FieldType} -- {field.Name} -- {field.GetValue(s)}");
+            }
+
+            foreach (var field in type.GetFields())
+            {
+                if (field.Name.Equals("age"))
+                {
+                    field.SetValue(s, 88);
+                }
+            }
+
+            foreach (var field in type.GetFields())
+            {
+                Console.WriteLine($"{field.FieldType} -- {field.Name} -- {field.GetValue(s)}");
+            }
+
+            Console.WriteLine(s.ToString());
+
+            // method 2 for get property
+            PropertyInfo property = type.GetProperty("Id");
+        }
+    }
+}
---
+++ 19_HandleAttributesByReflection/19_HandleAttributesByReflection/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_HandleAttributesByReflection
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int age;
+
+        public override string ToString()
+        {
+            return "Student " + Id + " " + Name + " " + Address + " " + age;
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine("Method 1 for {0}", this.GetType());
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method 2 for {0}", this.GetType());
+        }
+
+        public void Method2(string s)
+        {
+            Console.WriteLine("Method 2 for {0}", this.GetType());
+            Console.WriteLine("\t" + s);
+        }
+
+        public void Method2(string s, int i)
+        {
+            Console.WriteLine("Method 2 for {0}", this.GetType());
+            Console.WriteLine("\t" + s);
+            Console.WriteLine("\t" + i);
+        }
+
+        private void Method3()
+        {
+            Console.WriteLine("Method 3 for {0}", this.GetType());
+        }
+
+        private void Method3(int i)
+        {
+            Console.WriteLine("Method 3 for {0}", this.GetType());
+            Console.WriteLine("\t" + i);
+        }
+
+        public static void Method3(string s)
+        {
+            Console.WriteLine("Method 3 for {0}", typeof(Student));
+            Console.WriteLine("\t" + s);
+        }
+    }
+}
