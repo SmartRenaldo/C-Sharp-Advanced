acbcbfdcb28a71e8032b1968e04273dc2e1d9268
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-15 13:17:13 +0930

    thread intro

---
+++ 29_Thread/29_Thread/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _29_Thread
+{
+    public class Program
+    {
+        static void Main(string[] args)
+         {
+            /*for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("Main thread: " + i);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("BOOM!");*/
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+
+            Console.ReadLine();
+        }
+    }
+}
