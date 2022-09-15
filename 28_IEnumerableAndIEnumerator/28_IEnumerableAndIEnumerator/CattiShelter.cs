03f1161e35c0cb54f7445cc9ad9f4d7a5646b538
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-15 10:38:23 +0930

    IEnumerable and IEnumerator

---
+++ 28_IEnumerableAndIEnumerator/28_IEnumerableAndIEnumerator/CattiShelter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_IEnumerableAndIEnumerator
+{
+    public class CattiShelter : IEnumerable<Cat>
+    {
+        public List<Cat> cats;
+
+        public CattiShelter()
+        {
+            cats = new List<Cat>()
+            {
+                new Cat("Leili", 70),
+                new Cat("Hancook", 30),
+                new Cat("Tichi", 42),
+                new Cat("Zoro", 21)
+            };
+        }
+
+        IEnumerator<Cat> IEnumerable<Cat>.GetEnumerator()
+        {
+            return cats.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 28_IEnumerableAndIEnumerator/28_IEnumerableAndIEnumerator/Program.cs
+++ 28_IEnumerableAndIEnumerator/28_IEnumerableAndIEnumerator/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace _28_IEnumerableAndIEnumerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CattiShelter cattiShelter = new CattiShelter();
+            
+            foreach (Cat cat in cattiShelter)
+            {
+                cat.Treat(3);
+            }
+        }
+    }
+}
