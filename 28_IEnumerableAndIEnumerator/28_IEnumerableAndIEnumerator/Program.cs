d7cb8b3a3b950e91c00013fb2c0a351c8e130e04
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-15 10:58:53 +0930

    IEnumerable and IEnumerator

--- 28_IEnumerableAndIEnumerator/28_IEnumerableAndIEnumerator/Program.cs
+++ 28_IEnumerableAndIEnumerator/28_IEnumerableAndIEnumerator/Program.cs
@@ -5,10 +5,57 @@
         static void Main(string[] args)
         {
             CattiShelter cattiShelter = new CattiShelter();
-            
+
             foreach (Cat cat in cattiShelter)
             {
                 cat.Treat(3);
+            }
+
+            IEnumerable<double> doubles1 = GetCollection("1");
+
+            Console.WriteLine("doubles1");
+            foreach (double d in doubles1)
+            {
+                Console.WriteLine(d);
+            }
+
+            IEnumerable<double> doubles2 = GetCollection("2");
+            Console.WriteLine("doubles2");
+            foreach (double d in doubles2)
+            {
+                Console.WriteLine(d);
+            }
+
+            IEnumerable<double> doubles3 = GetCollection("3");
+            Console.WriteLine("doubles3");
+            foreach (double d in doubles3)
+            {
+                Console.WriteLine(d);
+            }
+        }
+
+        static IEnumerable<double> GetCollection(string s)
+        {
+
+
+
+            switch (s)
+            {
+                case "1":
+                    return new List<double>() { 1.1, 8.8, -9.7, -5.4 };
+
+                case "2":
+                    return new double[] { 5.5, 9.7, -14.8 };
+
+                default:
+                    Queue<double> queue = new Queue<double>();
+                    queue.Enqueue(3.3);
+                    queue.Enqueue(4.5);
+                    queue.Enqueue(-6.1);
+                    queue.Enqueue(-8.7);
+                    queue.Enqueue(10.8);
+
+                    return queue;
             }
         }
     }
