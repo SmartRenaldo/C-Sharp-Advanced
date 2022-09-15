45c6109b2d18cb2abf7cddae56a5b3640a17dc1e
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-15 10:22:10 +0930

    IEnumerable and IEnumerator

---
+++ 28_IEnumerableAndIEnumerator/28_IEnumerableAndIEnumerator/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_IEnumerableAndIEnumerator
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Cat (string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Treat(int numberOfTreats)
+        {
+            Console.WriteLine("Name: {0}, age: {1}, number of treats: {2}", Name, Age, numberOfTreats);
+        }
+    }
+}
