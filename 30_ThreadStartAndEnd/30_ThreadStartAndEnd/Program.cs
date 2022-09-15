33094624e28733316bedfad8830add005c1309b7
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-15 13:49:15 +0930

    thread start and end

---
+++ 30_ThreadStartAndEnd/30_ThreadStartAndEnd/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _30_ThreadStartAndEnd;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var taskCompletionSource = new TaskCompletionSource<int>();
+
+        var thread = new Thread(() =>
+        {
+            Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+            Thread.Sleep(1000);
+            taskCompletionSource.TrySetResult(0);
+        });
+
+        thread.Start();
+        var test = taskCompletionSource.Task.Result;
+        Console.WriteLine("Task was done: {0}", test);
+    }
+}
