859a0df772f5b25c69b3cae12f428b238546dcaa
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-15 13:50:10 +0930

    threadPool

---
+++ 31_ThreadPool/31_ThreadPool/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _31_ThreadPool;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //100 threads in pool
+        Enumerable.Range(0, 100).ToList().ForEach(x =>
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine($"Managed thread id: {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(100);
+                Console.WriteLine($"Managed thread id: {Thread.CurrentThread.ManagedThreadId} ended");
+            });
+            /*new Thread(() =>
+            {
+                Console.WriteLine($"Managed thread id: {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(500);
+                Console.WriteLine($"Managed thread id: {Thread.CurrentThread.ManagedThreadId} ended");
+            }).Start();*/
+        });
+
+        Console.ReadKey();
+    }
+}
